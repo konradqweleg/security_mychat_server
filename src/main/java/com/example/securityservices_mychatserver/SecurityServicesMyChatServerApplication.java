d7d1f99893718ce2f943d92e7e78186b97ab9f7f
Konrad <konradnrog@gmail.com>
2023-09-22 20:52:51 +0200

    first commit

---
+++ src/main/java/com/example/securityservices_mychatserver/SecurityServicesMyChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityservices_mychatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityServicesMyChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityServicesMyChatServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/securityservices_mychatserver/SecurityServicesMyChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityservices_mychatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityServicesMyChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
